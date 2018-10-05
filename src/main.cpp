79a996e88c56977f28fc72abd02910c5d4d3fdec
Alexander Elfimow <my.free.mail@gmx.de>
2018-10-05 22:20:10 +0200

    Introduced function

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,17 @@ static_assert(sizeof(size_t) == 8);
 
 extern "C" void clflush_func(void *p1, void *p2);
 
+template <class T> void rowhammer(T &data1, T &data2)
+{
+    for (auto &d1: data1)
+    {
+        for (auto &d2: data2)
+        {
+            ::clflush_func(&d1, &d2);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -36,28 +47,15 @@ try
     for (size_t step = 0; step < maxSteps; ++step)
     {
         std::cout << "Step: " << step << std::endl;
-        {
-            scoped_chrono sc { "Duration (d1, d2): " };
 
-            for (auto &d1: data1)
-            {
-                for (auto &d2: data2)
-                {
-                    clflush_func(&d1, &d2);
-                }
-            }
+        {
+            scoped_chrono sc { "Duration of rowhammer (d1, d2): " };
+            rowhammer(data1, data2);
         }
 
         {
-            scoped_chrono sc { "Duration (d2, d1): " };
-
-            for (auto &d2: data2)
-            {
-                for (auto &d1: data1)
-                {
-                    clflush_func(&d1, &d2);
-                }
-            }
+            scoped_chrono sc { "Duration of rowhammer (d2, d1): " };
+            rowhammer(data2, data1);
         }
     }
 
