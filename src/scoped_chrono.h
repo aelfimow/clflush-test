66671b57831e3f5d70790fee85bf50ef40334b31
Alexander Elfimow <my.free.mail@gmx.de>
2018-10-05 22:09:31 +0200

    Moved deleted methods

--- src/scoped_chrono.h
+++ src/scoped_chrono.h
@@ -9,17 +9,17 @@ class scoped_chrono
 {
     public:
         explicit scoped_chrono(const std::string &str);
+        ~scoped_chrono();
 
+    private:
+        scoped_chrono_priv * const m_priv;
+
+    public:
         scoped_chrono() = delete;
         scoped_chrono(const scoped_chrono &instance) = delete;
         scoped_chrono(const scoped_chrono &&instance) = delete;
         scoped_chrono &operator=(const scoped_chrono &instance) = delete;
         scoped_chrono &operator=(const scoped_chrono &&instance) = delete;
-
-        ~scoped_chrono();
-
-    private:
-        scoped_chrono_priv * const m_priv;
 };
 
 #endif
--- src/scoped_chrono_priv.h
+++ src/scoped_chrono_priv.h
@@ -7,18 +7,18 @@ class scoped_chrono_priv
 {
     public:
         explicit scoped_chrono_priv(const std::string &str);
-
-        scoped_chrono_priv() = delete;
-        scoped_chrono_priv(const scoped_chrono_priv &instance) = delete;
-        scoped_chrono_priv(const scoped_chrono_priv &&instance) = delete;
-        scoped_chrono_priv &operator=(const scoped_chrono_priv &instance) = delete;
-        scoped_chrono_priv &operator=(const scoped_chrono_priv &&instance) = delete;
-
         ~scoped_chrono_priv();
 
     private:
         const std::string m_message;
         const std::chrono::high_resolution_clock::time_point m_now;
+
+    public:
+        scoped_chrono_priv() = delete;
+        scoped_chrono_priv(const scoped_chrono_priv &instance) = delete;
+        scoped_chrono_priv(const scoped_chrono_priv &&instance) = delete;
+        scoped_chrono_priv &operator=(const scoped_chrono_priv &instance) = delete;
+        scoped_chrono_priv &operator=(const scoped_chrono_priv &&instance) = delete;
 };
 
 #endif
